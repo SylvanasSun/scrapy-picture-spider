241baf96f69d3f874028ca3afbc6b543a89b9b65
SylvanasSun <sylvanassun_xtz@163.com>
2017-09-17 20:26:29 +0800

    Add function post_login

--- pixiv_spider/spiders/pixiv_spider.py
+++ pixiv_spider/spiders/pixiv_spider.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.extensions.closespider import CloseSpider
 from scrapy.http import Request, FormRequest
 from pixiv_spider.items import PixivSpiderItem
 
@@ -12,7 +13,8 @@ class PixivSpider(CrawlSpider):
     start_urls = ['https://www.pixiv.net/']
 
     rules = (
-        Rule(LxmlLinkExtractor(allow=('/ranking.php?mode=daily/\w*+',)), callback='', follow=True)
+        # just crawl daily recommend
+        Rule(LxmlLinkExtractor(allow=('.+?mode=daily.*',)), callback='parse_page', follow=True)
     )
 
     headers = {
@@ -22,17 +24,45 @@ class PixivSpider(CrawlSpider):
         "Connection": "keep-alive",
         "Content-Type": " application/x-www-form-urlencoded; charset=UTF-8",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
-        "Referer": "https://www.pixiv.net/"
+        "Referer": "https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index"
     }
 
     # Override function start_requests for realize custom request
     def start_requests(self):
+        # cookiejar is param of cookie Middleware
+        # it need to keep passing it along on subsequent requests
         return [
             Request(url='https://accounts.pixiv.net/login', meta={'cookiejar': 1}, callback=self.post_login)
         ]
 
-    def __init__(self, username, password, *args, **kwargs):
+    # username and password from command line input
+    def __init__(self, username=None, password=None, *args, **kwargs):
         super(PixivSpider, self).__init__(*args, **kwargs)
         self.username = username
         self.password = password
 
+    def post_login(self, response):
+        # check username and password
+        if self.username is None or self.password is None:
+            raise CloseSpider('username or password is null!')
+
+        print('Preparing login, username = %s password = %s' % (self.username, self.password))
+        post_key = response.css('#old-login input[name=post_key]::attr(value)').extract_first()
+
+        # FormRequest for dealing with HTML forms
+        # function from_response for simulate a user login
+        return FormRequest.from_response(
+            response,
+            meta={'cookiejar': response.meta['cookiejar']},
+            headers=self.headers,
+            formdata={
+                'pixiv_id': self.username,
+                'password': self.password,
+                'post_key': post_key,
+                'mode': 'login'
+            },
+            callable=self.after_login,
+            dont_filter=True
+        )
+
+    
